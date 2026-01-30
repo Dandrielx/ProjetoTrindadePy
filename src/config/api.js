1a3825927ad583774c52ed3f2eb84c88796cbaee
Dionys Eicholz <98618030+Dandrielx@users.noreply.github.com>
2026-01-30 18:23:33 -0300

    Switch API base URL to production endpoint

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 // Exemplo de como definir (ajuste a porta se necessário)
 // Se estiver testando com emulador Android e o backend no mesmo PC:
-export const API_BASE_URL = 'http://192.168.18.120:5000'; // URL do backend
-//export const API_BASE_URL = 'https://projetotrindadepybackend.onrender.com'; // URL do backend em produção
+//export const API_BASE_URL = 'http://192.168.18.120:5000'; // URL do backend
+export const API_BASE_URL = 'https://projetotrindadepybackend.onrender.com'; // URL do backend em produção
